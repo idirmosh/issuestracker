e8d4afe72cd5b0aba999715f5db335f0ad2ffc02
idirmosh <idir.mosh@gmail.com>
2022-09-22 09:03:00 -0400

    init

--- packages/ui/components/Comment/SingleComment.tsx
+++ packages/ui/components/Comment/SingleComment.tsx
@@ -4,7 +4,13 @@ import { formatDate } from "shared/libs/helpers";
 import { Comment } from "shared/types";
 import { AuthContext } from "../../context/AuthContext";
 import useOnClickOutside from "../../hooks/useOnClickOutside";
-import { ChatIcon, CheckIcon, PencelIcon } from "../../icons";
+import {
+  ChatIcon,
+  CheckIcon,
+  CheckNoCircleIcon,
+  EditDotsIcon,
+  PencelIcon,
+} from "../../icons";
 
 interface ISCProps {
   comment: Comment;
@@ -15,10 +21,14 @@ export default function SingleComment({ comment, contextUserId }: ISCProps) {
   const singleCommentRef = useRef<HTMLInputElement>(null);
   const canEdit: boolean = contextUserId === comment?.user?.id;
   const [editMode, setEditMode] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const [commentText, setCommentText] = useState<string>(comment?.content);
 
   const onEdit = () => {
     setEditMode(true);
+    setIsOpen(false);
+    singleCommentRef.current.childNodes[1].childNodes[0].autofocus = true;
+    // console.dir(singleCommentRef.current.childNodes[1].childNodes[0].autofocus);
   };
 
   useOnClickOutside(singleCommentRef, () => setEditMode(false));
@@ -68,15 +78,36 @@ export default function SingleComment({ comment, contextUserId }: ISCProps) {
           ) : (
             <div className="w-full">
               <textarea
-                autoFocus={true}
+                autoFocus={editMode}
                 value={commentText}
                 onChange={(e: any) => setCommentText(e.target.value)}
                 className="border-px ml-1 block w-full resize-none rounded-lg border-gray-300 p-2.5 text-sm text-gray-900 focus:border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-300"
               />
             </div>
           )}
+          <div className="relative">
+            <div
+              onClick={() => setIsOpen(true)}
+              className="flex h-7 w-7 cursor-pointer items-center justify-around rounded-full border border-gray-200 bg-gray-50 hover:border-gray-300 hover:bg-gray-100"
+            >
+              <EditDotsIcon className="h-4 w-4 fill-gray-700" />
+            </div>
+            {isOpen && (
+              <SingleCommentAction
+                commentId={comment.id}
+                setEditMode={setEditMode}
+                setIsOpen={setIsOpen}
+              />
+            )}
 
-          {canEdit && (
+            {editMode && (
+              <div className="mt-2 flex h-7 w-7 cursor-pointer items-center justify-around rounded-full border border-blue-200 bg-blue-50 hover:border-blue-300 hover:bg-blue-100">
+                <CheckNoCircleIcon className="h-4 w-4 fill-blue-500" />
+              </div>
+            )}
+          </div>
+
+          {/* {canEdit && (
             <div
               onClick={onEdit}
               className="flex cursor-pointer items-center rounded-md border border-gray-200 py-1 px-2 text-xs text-gray-900 hover:border-gray-400 hover:shadow"
@@ -93,9 +124,34 @@ export default function SingleComment({ comment, contextUserId }: ISCProps) {
                 </Fragment>
               )}
             </div>
-          )}
+          )} */}
         </div>
       </div>
     </li>
+  );
+}
+
+function SingleCommentAction({ commentId, setEditMode, setIsOpen }: any) {
+  const onEdit = () => {
+    setEditMode(true);
+    setIsOpen(false);
+  };
+
+  const onDelete = () => {};
+  return (
+    <ul className="absolute right-3 z-10 w-28 cursor-pointer rounded-md border border-gray-200 bg-white text-sm font-medium shadow-lg">
+      <li
+        onClick={onEdit}
+        className="border-b border-gray-100 px-3 py-2 text-gray-800 hover:bg-gray-50"
+      >
+        Edit
+      </li>
+      <li
+        onClick={onDelete}
+        className="px-3 py-2 text-red-500 hover:bg-gray-50"
+      >
+        Delete
+      </li>
+    </ul>
   );
 }
--- packages/ui/icons.tsx
+++ packages/ui/icons.tsx
@@ -240,6 +240,22 @@ export const CheckIcon = (props: any): JSX.Element => {
     </svg>
   );
 };
+export const CheckNoCircleIcon = (props: any): JSX.Element => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path
+        fillRule="evenodd"
+        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
 
 export const CircleIcon = (props: any): JSX.Element => {
   return (
